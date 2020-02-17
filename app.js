7a9af6fa7182958f6f6bf0e622e9ef17c2979e22
yashmehtaaimdek <59681231+yashmehtaaimdek@users.noreply.github.com>
2020-02-17 16:55:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,137 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//  next(createError(404));
+//});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// default route
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'hello' })
+});
+
+// connection configurations
+var dbConn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'node_js_api'
+});
+
+// connect to database
+dbConn.connect();
+
+// Retrieve all users
+app.get('/users', function (req, res) {
+    console.log("Inside /users");
+    dbConn.query('SELECT * FROM users', function (error, results) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'users list.' });
+    });
+});
+
+// Retrieve user with id
+app.get('/user/:id', function (req, res) {
+
+    let user_id = req.params.id;
+
+    if (!user_id) {
+        return res.status(400).send({ error: true, message: 'Please provide user_id' });
+    }
+
+    dbConn.query('SELECT * FROM users where id=?', user_id, function (error, results) {
+        if (error) throw error;
+        return res.send({ error: false, data: results[0], message: 'users list.' });
+    });
+
+});
+
+// Add a new user
+app.post('/user', function (req, res) {
+
+    let user = req.body;
+
+    if (!user) {
+        return res.status(400).send({ error:true, message: 'Please provide user' });
+    }
+
+    dbConn.query("INSERT INTO users (id,name,email,timestamp) values (?,?,?,?)", [user.id,user.name,user.email,user.timestap], function (error, results) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
+    });
+});
+
+//  Update user with id
+app.put('/user', function (req, res) {
+
+    let user_id = req.body.user_id;
+    let user = req.body.user;
+
+    if (!user_id || !user) {
+        return res.status(400).send({ error: user, message: 'Please provide user and user_id' });
+    }
+
+    dbConn.query("UPDATE users SET user = ? WHERE id = ?", [user, user_id], function (error, results) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'user has been updated successfully.' });
+    });
+});
+
+//  Delete user
+app.delete('/user', function (req, res) {
+
+    let user_id = req.body.user_id;
+
+    if (!user_id) {
+        return res.status(400).send({ error: true, message: 'Please provide user_id' });
+    }
+    dbConn.query('DELETE FROM users WHERE id = ?', [user_id], function (error, results) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'User has been updated successfully.' });
+    });
+});
+
+
+module.exports = app;
